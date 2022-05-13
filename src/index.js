9c3c0d2c68b836d1ef963acec05fb30047b8c022
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-05-13 15:59:54 -0500

    configuracion DB - estructuracion proyecto

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const morgan = require("morgan");
 const session = require('express-session')
 const flash = require('express-flash')
 
+//Enrutadores
+const rutasUsers = require('./routes/users')
+const rutasProducts = require('./routes/products')
+
 const app = express();
 
 app.use(cors())
@@ -12,9 +16,15 @@ app.use(express.json());
 app.use(session({ secret: 'token-muy-secreto', resave: true, saveUninitialized: true }));
 app.use(flash())
 
+// routes
+app.use('/products', rutasProducts);
+app.use('/users', rutasUsers);
+
+/*
 app.get('/', (req, res) => {
   res.send("Bienvenido al Servidor")
 })
+*/
 
 app.listen(9000, function(){
   console.log("Servidor iniciado port 9000");
